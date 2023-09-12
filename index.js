38b850ba909897b3f266046c8b1e1d4814a1bcb1
AliNaqvi5 <84670143+AliNaqvi5@users.noreply.github.com>
2023-09-12 07:45:14 +0500

    AXIOS DATAUPLOAD TASK

--- index.js
+++ index.js
@@ -2,22 +2,33 @@ const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
 const axios = require('axios');
 const port = new SerialPort({ path: 'COM3', baudRate: 9600 })
+
 let LoggedIn = false;
 let receivedData = '';
 let sendData = [];
+
 const sensorsId = {"temperature" : 1,"humidity":2,"lux" :3 ,"soilMoisture": 4,"rainStatus":5};
 const sensorsUnit = {"temperature" : "Celsius","humidity":"%","lux" :"lux" ,"soilMoisture": "%","rainStatus":"NO Unit"};
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 parser.on('data', (data) => {
     // Append the received data to the accumulated string
-    receivedData += "$$"+data;
+    // receivedData += "$$"+data;
 
-    console.log("inside parser : ",data);
-    data = data.split(":");
-    console.log("inside parser : ",sensorsId[data[0]]);
-    sendData = {"sensor_id": sensorsId[data[0]] , "value" : data[1] , "unit": sensorsUnit[data[0]] , "location": "KHI"};
-    sendDataApi(sendData);
+    console.log("Data Received : ",data);
+    if(!data.includes("Alarm")) {
+        data = data.split(":");
+        console.log("inside parser : ", sensorsId[data[0]]);
+        sendData = {"sensor_id": sensorsId[data[0]], "value": data[1], "unit": sensorsUnit[data[0]], "location": "KHI"};
+        sendDataApi(sendData);
+    } else if(data.includes("Alarm"))
+    {
+        data = data.split("Alarm");
+        data = data[1].split(":");
+        console.log("Alarm : ", sensorsId[data[0]]);
+        sendData = {"sensor_id": sensorsId[data[0]], "title": "Alarm"};
+        sendDataApiAlarms(sendData);
+    }
     // Check if the received data contains a complete message (e.g., terminated by a newline)
 
 });
@@ -49,6 +60,15 @@ axios.post(apiUrl, dataToSend, { headers })
 async function sendDataApi(dataToS) {
     try {
         const response = await axios.post("http://127.0.0.1:8000/api/store", dataToS, { headers });
+        console.log('Response:', response.data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+async function sendDataApiAlarms(dataToS) {
+    try {
+        const response = await axios.post("http://127.0.0.1:8000/api/alarmstore", dataToS, { headers });
         console.log('Response:', response.data);
     } catch (error) {
         console.error('Error:', error);
